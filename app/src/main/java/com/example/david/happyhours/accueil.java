ca6886412a1a332939f3459a13eef336d4d4b9fe
DavidMarck <david.marck88@gmail.com>
2016-04-26 21:05:56 +0200

    Ajout d'un Drawer dans la Toolbar

--- app/src/main/java/com/example/david/happyhours/accueil.java
+++ app/src/main/java/com/example/david/happyhours/accueil.java
@@ -1,5 +1,9 @@
 package com.example.david.happyhours;
 
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +11,9 @@ import android.support.v7.widget.Toolbar;
 public class accueil extends AppCompatActivity {
 
     private Toolbar toolbar;
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +24,31 @@ public class accueil extends AppCompatActivity {
     }
 
     public void initialisations() {
+
+        // Instanciation de la Toolbar
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // On définit notre Toolbar en tant qu'ActionBar
         setSupportActionBar(toolbar);
+
+        //afficher le bouton retour
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Instanciation d'un Drawer
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        this.drawerToggle = new ActionBarDrawerToggle(this,this.drawerLayout,0,0);
+        this.drawerLayout.addDrawerListener(this.drawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // synchroniser le drawerToggle après la restauration via onRestoreInstanceState
+        drawerToggle.syncState();
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 }
