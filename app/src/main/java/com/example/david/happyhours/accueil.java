f0b69b68e90cab1612372ff9ec8fea6647a99c3e
DavidMarck <david.marck88@gmail.com>
2016-04-26 20:14:37 +0200

    Recodage toolbar et replacement

--- app/src/main/java/com/example/david/happyhours/accueil.java
+++ app/src/main/java/com/example/david/happyhours/accueil.java
@@ -2,12 +2,22 @@ package com.example.david.happyhours;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class accueil extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+
+        initialisations();
+    }
+
+    public void initialisations() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
