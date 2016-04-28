eb45ebaf0d49f7bd0047476a6f568592c17b38a4
DavidMarck <david.marck88@gmail.com>
2016-04-28 17:12:21 +0200

    Ajout SearchView dans Toolbar

--- app/src/main/java/com/example/david/happyhours/accueil.java
+++ app/src/main/java/com/example/david/happyhours/accueil.java
@@ -1,12 +1,17 @@
 package com.example.david.happyhours;
 
 import android.content.res.Configuration;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class accueil extends AppCompatActivity {
 
@@ -23,6 +28,58 @@ public class accueil extends AppCompatActivity {
         initialisations();
     }
 
+    @Override
+    /*
+    * Méthode d'initialisation des actions du menu (dans la toolbar)
+    * */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //ajoute les entrées de menu_toolbar à l'ActionBar (transformée en toolbar)
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // code de gestion quand click recherche
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+        return true;
+    }
+
+    //gère le click sur une action de l'ActionBar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            // Gérer les 'case' ici
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // synchroniser le drawerToggle après la restauration via onRestoreInstanceState
+        drawerToggle.syncState();
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    /*
+    * Méthode d'initialisation des composants graphiques de l'interface de l'activité
+    * */
     public void initialisations() {
 
         // Instanciation de la Toolbar
@@ -38,17 +95,5 @@ public class accueil extends AppCompatActivity {
         this.drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         this.drawerToggle = new ActionBarDrawerToggle(this,this.drawerLayout,0,0);
         this.drawerLayout.addDrawerListener(this.drawerToggle);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // synchroniser le drawerToggle après la restauration via onRestoreInstanceState
-        drawerToggle.syncState();
-    }
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        drawerToggle.onConfigurationChanged(newConfig);
     }
 }
