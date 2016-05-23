98ada57c91142b2a414f7ae2d54b11cb998b9740
DavidMarck <david.marck88@gmail.com>
2016-05-23 21:02:48 +0200

    Ajout BarAdapter.java + BarRow.java (à continuer)

---
+++ app/src/main/java/com/example/david/happyhours/BarAdapter.java
@@ -0,0 +1,66 @@
+package com.example.david.happyhours;
+
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by David on 23/05/2016.
+ */
+public class BarAdapter extends ArrayAdapter<BarRow> {
+
+    /**
+     * Constructeur
+     *
+     * @param context
+     * @param bars liste contenant toutes les informations relatives aux bars à afficher
+     */
+    public BarAdapter(Context context, List<BarRow> bars) {
+        super(context,0,bars);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.row_bar,parent, false);
+        }
+
+        BarViewHolder viewHolder = (BarViewHolder) convertView.getTag();
+        if(viewHolder == null){
+            viewHolder = new BarViewHolder();
+            viewHolder.bar_nom = (TextView) convertView.findViewById(R.id.bar_nom);
+            viewHolder.bar_hh = (TextView) convertView.findViewById(R.id.bar_hh);
+            viewHolder.bar_adresse = (TextView) convertView.findViewById(R.id.bar_adresse);
+            viewHolder.bar_horaires = (TextView) convertView.findViewById(R.id.bar_horaires);
+            viewHolder.btn_favori = (ImageButton) convertView.findViewById(R.id.btn_favori);
+            viewHolder.btn_barathon = (ImageButton) convertView.findViewById(R.id.btn_barathon);
+            viewHolder.btn_geoloc = (ImageButton) convertView.findViewById(R.id.btn_geoloc);
+            convertView.setTag(viewHolder);
+        }
+
+        //getItem(position) va récupérer l'item [position] de la List<Tweet> tweets
+        BarRow barRow = getItem(position);
+
+        //il ne reste plus qu'à remplir notre vue
+        viewHolder.bar_nom.setText(barRow.getNom());
+        viewHolder.bar_hh.setText(barRow.getHappy_hours());
+        viewHolder.bar_adresse.setText(barRow.getAdresse());
+        viewHolder.bar_horaires.setText(barRow.getHoraires());
+        viewHolder.bar_adresse.setText(barRow.getAdresse());
+
+        return convertView;
+    }
+
+    private class BarViewHolder {
+        public TextView bar_nom,bar_hh,bar_adresse,bar_horaires;
+        public ImageButton btn_favori,btn_barathon,btn_geoloc;
+    }
+}
---
+++ app/src/main/java/com/example/david/happyhours/BarRow.java
@@ -0,0 +1,63 @@
+package com.example.david.happyhours;
+
+/**
+ * Created by David on 23/05/2016.
+ *
+ * Classe objet modèle contenant les informations affichées
+ * dans la ListView de l'activité accueil.
+ */
+public class BarRow {
+
+    private String nom,happy_hours,adresse,horaires;
+
+    /**
+     * Constructeur
+     *
+     * @param nom
+     * @param hh_deb
+     * @param hh_fin
+     * @param adresse
+     * @param hor_ouv
+     * @param hor_ferm
+     */
+    public BarRow(String nom, String hh_deb, String hh_fin, String adresse, String hor_ouv, String hor_ferm) {
+        this.nom = nom;
+        this.happy_hours = hh_deb + " - " + hh_fin;
+        this.adresse = adresse;
+        this.horaires = hor_ouv + " - " + hor_ferm;
+    }
+
+    // Getters & Setters
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getHappy_hours() {
+        return happy_hours;
+    }
+
+    public void setHappy_hours(String happy_hours) {
+        this.happy_hours = happy_hours;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getHoraires() {
+        return horaires;
+    }
+
+    public void setHoraires(String horaires) {
+        this.horaires = horaires;
+    }
+}
